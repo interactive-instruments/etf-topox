2ca3ffeddecf7865f2dc0bf89b8dcc3a03e80c23
Jon Herrmann <herrmann@interactive-instruments.de>
2018-10-09 11:39:05 +0200

    Data driven testing

---
+++ src/test/java/de/interactive_instruments/etf/bsxm/BsxDataDrivenTest.java
@@ -0,0 +1,140 @@
+/**
+ * Copyright 2010-2018 interactive instruments GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.interactive_instruments.etf.bsxm;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.basex.core.cmd.XQuery;
+import org.xmlunit.builder.DiffBuilder;
+import org.xmlunit.builder.Input;
+import org.xmlunit.diff.Diff;
+import org.xmlunit.diff.Difference;
+
+import de.interactive_instruments.IFile;
+import de.interactive_instruments.io.GmlAndXmlFilter;
+import de.interactive_instruments.io.MultiFileFilter;
+
+/**
+ * @author Jon Herrmann ( herrmann aT interactive-instruments doT de )
+ */
+class BsxDataDrivenTest {
+	private final IFile[] dataFiles;
+	private final IFile queryDir;
+	private final IFile defaultQueryFile;
+	private final IFile[] expectedFiles;
+	private final XmlUnitDetailFormatter formatter = new XmlUnitDetailFormatter();
+
+	public class DDTResBundle {
+		final int pos;
+
+		private DDTResBundle(final int pos) {
+			this.pos = pos;
+		}
+
+		public String getDataPath() {
+			return dataFiles[pos].getAbsolutePath();
+		}
+
+		public XQuery getQuery() {
+			try {
+				final String queryFileName = dataFiles[pos].getFilenameWithoutExt() + ".xq";
+				final IFile queryFile = queryDir.secureExpandPathDown(queryFileName);
+				if (queryFile.exists()) {
+					return new XQuery(queryFile.readContent().toString());
+				} else {
+					return new XQuery(defaultQueryFile.readContent().toString());
+				}
+			} catch (final IOException e) {
+				fail("Error loading query file ", e);
+				return null;
+			}
+		}
+
+		public void compare(final IFile resultFile) {
+			String expectedXml = null;
+			try {
+				expectedXml = new IFile(expectedFiles[pos]).readContent("UTF-8").toString();
+			} catch (final IOException e) {
+				fail("Could not read " + expectedFiles[pos].getAbsolutePath(), e);
+			}
+			String resultXml = null;
+			try {
+				resultXml = resultFile.readContent("UTF-8").toString();
+			} catch (final IOException e) {
+				fail("Could not read " + resultFile.getAbsolutePath());
+			}
+
+			final Diff diff = DiffBuilder.compare(Input.fromString(resultXml))
+					.withTest(Input.fromString(expectedXml))
+					.checkForSimilar().checkForIdentical()
+					.ignoreComments()
+					.ignoreWhitespace()
+					.normalizeWhitespace()
+					.ignoreElementContentWhitespace()
+					.build();
+
+
+			if (diff.hasDifferences()) {
+				final Difference difference = diff.getDifferences().iterator().next();
+				assertEquals(formatter.getControlDetailDescription(difference.getComparison()),
+						formatter.getTestDetailDescription(difference.getComparison()));
+			}
+		}
+
+		public String getName() {
+			try {
+				return dataFiles[pos].getFilenameWithoutExt();
+			} catch (IOException e) {
+				return Integer.toString(pos);
+			}
+		}
+
+		@Override
+		public String toString() {
+			return getName();
+		}
+	}
+
+	private BsxDataDrivenTest(final IFile ddtDirectory) {
+		final MultiFileFilter xmlFilter = GmlAndXmlFilter.instance().filename();
+		dataFiles = ddtDirectory.secureExpandPathDown("data").listIFiles(xmlFilter);
+		queryDir = ddtDirectory.secureExpandPathDown("queries");
+		defaultQueryFile = queryDir.secureExpandPathDown("default.xq");
+		assertTrue(defaultQueryFile.exists(), "Default XQuery file missing");
+		expectedFiles = ddtDirectory.secureExpandPathDown("expected").listIFiles(xmlFilter);
+		assertTrue(dataFiles.length > 0, "No XML files found in test data directory");
+		assertEquals(dataFiles.length, expectedFiles.length,
+				"Number of test data files does not match the number of expected result files");
+	}
+
+	private ArrayList<DDTResBundle> getDDTResBundles() {
+		return IntStream.range(0, dataFiles.length).mapToObj(DDTResBundle::new)
+				.collect(Collectors.toCollection(() -> new ArrayList<>(dataFiles.length)));
+	}
+
+	public static List<DDTResBundle> createDDT(final IFile ddtDirectory) {
+		return new BsxDataDrivenTest(ddtDirectory).getDDTResBundles();
+	}
+
+}
---
+++ src/test/java/de/interactive_instruments/etf/bsxm/TopoXTest.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright 2010-2018 interactive instruments GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.interactive_instruments.etf.bsxm;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.stream.Stream;
+
+import org.basex.core.BaseXException;
+import org.basex.core.Context;
+import org.basex.core.cmd.CreateDB;
+import org.basex.core.cmd.DropDB;
+import org.basex.core.cmd.XQuery;
+import org.basex.query.QueryException;
+import org.basex.query.util.pkg.RepoManager;
+import org.junit.jupiter.api.*;
+
+import de.interactive_instruments.IFile;
+import de.interactive_instruments.exceptions.ExcUtils;
+
+/**
+ * @author Jon Herrmann ( herrmann aT interactive-instruments doT de )
+ */
+public class TopoXTest {
+
+	private Context ctx = new Context();
+
+	private final static IFile ddtDirectory = new IFile("src/test/resources/ddt");
+	private final static IFile outputDirectory = new IFile("build/tmp/ddt/results");
+	private final static IFile tmpOutputDirectory = new IFile("build/tmp/ddt/tmp_outputs");
+	private final static String TOPOX_INSTALL_PATH = "build/distributions/TopoX.xar";
+	private final static String DB_NAME = "TOPOX-JUNIT-TEST-DB-000";
+
+	@BeforeAll
+	static void setUp() throws QueryException, IOException {
+		final RepoManager repoManger = new RepoManager(new Context());
+		try {
+			repoManger.delete("ETF TopoX");
+		} catch (QueryException ign) {
+			ExcUtils.suppress(ign);
+		}
+		repoManger.install(TOPOX_INSTALL_PATH);
+		outputDirectory.ensureDir();
+		tmpOutputDirectory.ensureDir();
+	}
+
+	@AfterEach
+	@BeforeEach
+	void dropDb() throws BaseXException {
+		new DropDB(DB_NAME).execute(ctx);
+	}
+
+	private OutputStream outputStream(final IFile outputFile) {
+		try {
+			return new FileOutputStream(outputFile);
+		} catch (FileNotFoundException e) {
+			fail("Error writing to file", e);
+			return null;
+		}
+	}
+
+	@TestFactory
+	Stream<DynamicTest> createTests() {
+		return BsxDataDrivenTest.createDDT(ddtDirectory).stream()
+				.map(t -> DynamicTest.dynamicTest(
+						"Data Test: " + t.getName(),
+						() -> {
+							final IFile testTmpOutputDirectory = tmpOutputDirectory.secureExpandPathDown(t.getName())
+									.ensureDir();
+							new CreateDB(DB_NAME, t.getDataPath()).execute(ctx);
+							final XQuery xQuery = t.getQuery();
+							xQuery.bind("$tmpOutputDirectory", testTmpOutputDirectory.getAbsolutePath());
+							xQuery.bind("$dbname", DB_NAME);
+							final IFile outputFile = new IFile(outputDirectory, t.getName() + ".xml");
+							xQuery.execute(ctx, outputStream(outputFile));
+							t.compare(outputFile);
+						}));
+	}
+
+}
